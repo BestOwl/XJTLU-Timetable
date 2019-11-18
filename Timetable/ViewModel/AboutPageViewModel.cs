73817309735eeb1f56e10b88ce8b3e48f09e7560
NightOwl <midnightow1@outlook.com>
2019-11-18 20:34:31 +0800

    Added AboutPage content

---
+++ Timetable/ViewModel/AboutPageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace Timetable.ViewModel
+{
+    public class AboutPageViewModel : BindableBase
+    {
+        public string VersionString
+        {
+            get => AppInfo.VersionString;
+        }
+    }
+}
