9a256024df046c5e42c61fe1f582731bf9bbdddd
NightOwl <midnightow1@outlook.com>
2019-11-25 19:45:09 +0800

    Update InstallStoreCert.ps1

--- Timetable.UWP/InstallStoreCert.ps1
+++ Timetable.UWP/InstallStoreCert.ps1
@@ -1,8 +1,7 @@
-Write-Host("Start to download store certificate from")
-$url = "${env:PrivateStorage_BaseUri}${env:PrivateStorage_FileToDownload}"
+Write-Host("Start to download store certificate from") 
 $header = @{
     Authorization = "${env:PrivateStorage_AccessToken}"
 }
 
-Invoke-WebRequest -Uri $url -UserAgent VSTS-Get -ContentType "application/json" -Method Get -Headers $header -OutFile "./StoreCert.pfx"
+Invoke-WebRequest -Uri "${env:PrivateStorage_BaseUri}${env:PrivateStorage_FileToDownload}" -UserAgent VSTS-Get -ContentType "application/json" -Method Get -Headers $header -OutFile "./StoreCert.pfx"
 Write-Host("Download completed")
