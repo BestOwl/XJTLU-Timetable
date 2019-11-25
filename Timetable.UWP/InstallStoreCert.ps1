3c0a7f5dc3fe36400bb60fa439eb15a90d493522
NightOwl <midnightow1@outlook.com>
2019-11-25 21:09:26 +0800

    test

--- Timetable.UWP/InstallStoreCert.ps1
+++ Timetable.UWP/InstallStoreCert.ps1
@@ -4,5 +4,5 @@ $header = @{
     Authorization = "${env:PrivateStorage_AccessToken}"
 }
 
-Invoke-WebRequest -Uri "https://goodtimestudio.visualstudio.com/PrivateStorage/_apis/git/repositories/PrivateStorage/items?api-version=1.0&scopePath=Timetable_UWP_Store.pfx" -UserAgent VSTS-Get -ContentType "application/json" -Method Get -Headers $header -OutFile "./StoreCert.pfx"
+Invoke-WebRequest -Uri "https://goodtimestudio.visualstudio.com/PrivateStorage/_apis/git/repositories/PrivateStorage/items?api-version=1.0&scopePath=Timetable_UWP_Store.pfx" -UserAgent "VSTS-Get" -ContentType "application/json" -Method Get -Headers $header -OutFile "./StoreCert.pfx"
 Write-Host("Download completed")
