0632e5cd685484bb9b5c8f19e57db3b1ce2f8cf9
NightOwl <midnightow1@outlook.com>
2019-11-25 18:26:16 +0800

    Updated UWP.yml

--- Timetable.UWP/InstallStoreCert.ps1
+++ Timetable.UWP/InstallStoreCert.ps1
@@ -3,6 +3,6 @@ $url = "${env:PrivateStorage_BaseUri}${env:PrivateStorage_FileToDownload}"
 $header = @{
     Authorization = "${env:PrivateStorage_AccessToken}"
 }
-Write-Host($url)
+
 Invoke-WebRequest -Uri $url -UserAgent VSTS-Get -ContentType "application/json" -Method Get -Headers $header -OutFile "./StoreCert.pfx"
 Write-Host("Download completed")
